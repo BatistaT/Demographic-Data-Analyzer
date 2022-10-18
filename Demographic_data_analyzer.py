78800edec1d98a58a39b810b80f7e10a02501f6c
Thiago Waslawick Batista <100001413+BatistaT@users.noreply.github.com>
2022-10-18 08:43:19 -0300

    Upload project

---
+++ Demographic_data_analyzer.py
@@ -0,0 +1,37 @@
+import pandas as pd
+
+def calculate_demographic_data(print_data = True):
+  df = None
+
+  race_count = None
+
+  average_age_men = None
+
+  percentage_bachelors = None
+
+  higher_education = None
+  lower_education = None
+
+  higher_education_rich = None
+  lower_education_rich = None
+
+  min_work_hours = None
+
+  num_min_workers = None
+
+  rich_percentage = None
+
+  top_IN_occupation = None
+
+
+  if print_data:
+    print("Number of each race:\n", race_count)
+    print("Average age of men:", average_age_men)
+    print(f"Percentage with Bachelors degrees: {percentage_bachelors}%")
+    print(f"Percentage with higher education that earn >50K: {higher_education_rich}%")
+    print(f"Percentage without higher education that earn >50K: {lower_education_rich}%")
+    print(f"Min work time: {min_work_hours} hours/week")
+    print(f"Percentage of rich among those who work fewest hours: {rich_percentage}%")
+    print("Top occupations in India:", top_IN_occupation)
+
+  return {'race_count': race_count, 'average_age_men': average_age_men, 'percentage_bachelors': percentage_bachelors, 'higher_education_rich': higher_education_rich, 'lower_education_rich': lower_education_rich, 'min_work_hours': min_work_hours, 'rich_percentage': rich_percentage, 'top_IN_occupation': top_IN_occupation}
---
+++ main.py
@@ -0,0 +1,4 @@
+import demographic_data_analyzer_SOLUTION
+from unittest import main
+
+demographic_data_analyzer_SOLUTION.calculate_demographic_data()
